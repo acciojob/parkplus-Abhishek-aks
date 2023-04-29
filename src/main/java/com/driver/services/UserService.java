8fbe157cf02ce5324dd3e55f8667eeb2b630f34c
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-04-29 22:34:58 +0530

    done

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -1,9 +1,11 @@
 package com.driver.services;
 
 
+import com.driver.model.User;
+
 public interface UserService {
 
-	void deleteUser(Integer userId);
-	User updatePassword(Integer userId, String password);
-    void register(String name, String phoneNumber, String password);
+	public void deleteUser(Integer userId);
+	public User updatePassword(Integer userId, String password);
+	public void register(String name, String phoneNumber, String password);
 }
